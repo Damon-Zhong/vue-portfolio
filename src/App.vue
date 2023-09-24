c0b868da6494f2727bc148cf71a0a6cdde6d9ada
Damon <damon@smart-one.ca>
2023-09-24 13:19:55 -0400

    added Icon component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <Avatar url="https://shop7.webmodule.prestashop.net/pokedoge/11831-large_default/snorlax.jpg" :size="100" />
+    <Icon type="home" />
   </div>
 </template>
 
 <script>
 import Avatar from './components/Avatar.vue'
+import Icon from './components/Icon.vue'
 
 export default {
   name: 'App',
   components: {
-    Avatar
+    Avatar,
+    Icon
   }
 }
 </script>
