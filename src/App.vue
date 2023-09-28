04db0ec2a866614ac4ed9b943d84c7836143599c
Damon <damon@smart-one.ca>
2023-09-27 23:21:02 -0400

    reorg repo

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,4 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <!-- <Avatar url="https://shop7.webmodule.prestashop.net/pokedoge/11831-large_default/snorlax.jpg" :size="100" />
-    <Icon type="home" /> -->
-    <Pager
-      :total="total"
-      :current="current"
-      :visibleNumber="visibleNumber"
-      @pageChange="handlePageChange($event)"
-    />
   </div>
 </template>
-
-<script>
-import Avatar from "./components/Avatar.vue";
-import Icon from "./components/Icon.vue";
-import Pager from "./components/Pager.vue";
-
-export default {
-  name: "App",
-  components: {
-    Pager,
-  },
-  data() {
-    return {
-      current: 1,
-      total: 302,
-      visibleNumber: 7,
-    };
-  },
-  methods: {
-    handlePageChange(newPageNum) {
-      this.current = newPageNum;
-      // 加载当前页数据
-    },
-  },
-};
-</script>
-
-<style lang="less" scoped>
-</style>
