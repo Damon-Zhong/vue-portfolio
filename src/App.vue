1890d913bb264a051230e72efe3b243d19a58c90
Damon <damon@smart-one.ca>
2023-09-28 11:19:12 -0400

    added Empty and ImageLoader components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,38 @@
 <template>
-  <div id="app">
+  <div>
+    <h1>App Root 组件</h1>
+    <div class="test-container">
+      <ImageLoader
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png"
+        placeholder="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?w=100"
+        @load="onImageLoaded"
+        :duration="3000"
+      />
+    </div>
   </div>
 </template>
+<script>
+import Pager from "./components/Pager";
+import Empty from "./components/Empty";
+import ImageLoader from "./components/ImageLoader";
+
+export default {
+  components: {
+    Pager,
+    Empty,
+    ImageLoader,
+  },
+  methods: {
+    onImageLoaded() {
+      console.log("图片加载完成");
+    },
+  },
+};
+</script>
+<style scoped>
+.test-container {
+  width: 500px;
+  height: 300px;
+  border: 1px solid red;
+}
+</style>
--- src/components/Pager/index.vue
+++ src/components/Pager/index.vue
@@ -1,138 +1,137 @@
 <template>
-    <div class="pager-container" v-if="pageNumber > 1">
-      <a @click="handlePageChange(1)" :class="{ disabled: current === 1 }"
-        >|&lt;&lt;</a
-      >
-      <a
-        @click="handlePageChange(current - 1)"
-        :class="{ disabled: current === 1 }"
-        >&lt;&lt;</a
-      >
-  
-      <a
-        v-for="(n, i) in currentDisplayPages"
-        :key="i"
-        :class="{ active: current === n }"
-        @click="handlePageChange(n)"
-        >{{ n }}</a
-      >
-  
-      <a
-        @click="handlePageChange(current + 1)"
-        :class="{ disabled: current === pageNumber }"
-        >&gt;&gt;</a
-      >
-      <a
-        @click="handlePageChange(pageNumber)"
-        :class="{ disabled: current === pageNumber }"
-        >&gt;&gt;|</a
-      >
-    </div>
-  </template>
+  <div class="pager-container" v-if="pageNumber > 1">
+    <a @click="handlePageChange(1)" :class="{ disabled: current === 1 }"
+      >|&lt;&lt;</a
+    >
+    <a
+      @click="handlePageChange(current - 1)"
+      :class="{ disabled: current === 1 }"
+      >&lt;&lt;</a
+    >
+
+    <a
+      v-for="(n, i) in currentDisplayPages"
+      :key="i"
+      :class="{ active: current === n }"
+      @click="handlePageChange(n)"
+      >{{ n }}</a
+    >
+
+    <a
+      @click="handlePageChange(current + 1)"
+      :class="{ disabled: current === pageNumber }"
+      >&gt;&gt;</a
+    >
+    <a
+      @click="handlePageChange(pageNumber)"
+      :class="{ disabled: current === pageNumber }"
+      >&gt;&gt;|</a
+    >
+  </div>
+</template>
   
   <script>
-  export default {
-    props: {
-      current: {
-        type: Number,
-        default: 1,
-      },
-      total: {
-        type: Number,
-        default: 100,
-      },
-      limit: {
-        type: Number,
-        default: 10,
-      },
-      visibleNumber: {
-        type: Number,
-        default: 10,
-      },
+export default {
+  props: {
+    current: {
+      type: Number,
+      default: 1,
     },
-    computed: {
-      // 总页数
-      pageNumber() {
-        return Math.ceil(this.total / this.limit);
-      },
-      // 可见的最小页码
-      minPageNum() {
-        let min = this.current - Math.floor(this.visibleNumber / 2);
-  
-        if (min < 1) {
-          min = 1;
-        }
-  
-        if (min > this.pageNumber - this.visibleNumber + 1) {
-          min = this.pageNumber - this.visibleNumber + 1;
-        }
-  
-        return min;
-      },
-      // 可见的最大页码
-      maxPageNum() {
-        let max = this.minPageNum + this.visibleNumber - 1;
-  
-        if (max > this.pageNumber) {
-          max = this.pageNumber;
-        }
-  
-        return max;
-      },
-      // 当前可见页码
-      currentDisplayPages() {
-        let pageNumberArr = [];
-  
-        for (let i = this.minPageNum; i <= this.maxPageNum; i++) {
-          pageNumberArr.push(i);
-        }
-  
-        return pageNumberArr;
-      },
+    total: {
+      type: Number,
+      default: 100,
     },
-    methods: {
-      handlePageChange(newPage) {
-        console.log("12313121", newPage);
-        if (newPage < 1) {
-          newPage = 1;
-        }
-  
-        if (newPage >= this.pageNumber) {
-          newPage = this.pageNumber;
-        }
-  
-        if (newPage === current) {
-          return;
-        }
-        this.$emit("pageChange", newPage);
-      },
+    limit: {
+      type: Number,
+      default: 10,
     },
-  };
-  </script>
-  
-  <style lang="less" scoped>
-  @import "~@/styles/colors.module.less";
-  
-  .pager-container {
-    display: flex;
-    justify-content: center;
-    margin: 20px 0;
-  
-    a {
-      color: @primary;
-      margin: 0 3px;
-      cursor: pointer;
-  
-      &.disabled {
-        color: @lightWords;
-        cursor: not-allowed;
+    visibleNumber: {
+      type: Number,
+      default: 10,
+    },
+  },
+  computed: {
+    // 总页数
+    pageNumber() {
+      return Math.ceil(this.total / this.limit);
+    },
+    // 可见的最小页码
+    minPageNum() {
+      let min = this.current - Math.floor(this.visibleNumber / 2);
+
+      if (min < 1) {
+        min = 1;
       }
-  
-      &.active {
-        color: @words;
-        font-weight: bold;
-        cursor: text;
+
+      if (min > this.pageNumber - this.visibleNumber + 1) {
+        min = this.pageNumber - this.visibleNumber + 1;
       }
+
+      return min;
+    },
+    // 可见的最大页码
+    maxPageNum() {
+      let max = this.minPageNum + this.visibleNumber - 1;
+
+      if (max > this.pageNumber) {
+        max = this.pageNumber;
+      }
+
+      return max;
+    },
+    // 当前可见页码
+    currentDisplayPages() {
+      let pageNumberArr = [];
+
+      for (let i = this.minPageNum; i <= this.maxPageNum; i++) {
+        pageNumberArr.push(i);
+      }
+
+      return pageNumberArr;
+    },
+  },
+  methods: {
+    handlePageChange(newPage) {
+      if (newPage < 1) {
+        newPage = 1;
+      }
+
+      if (newPage >= this.pageNumber) {
+        newPage = this.pageNumber;
+      }
+
+      if (newPage === current) {
+        return;
+      }
+      this.$emit("pageChange", newPage);
+    },
+  },
+};
+</script>
+  
+<style lang="less" scoped>
+@import "~@/styles/colors.module.less";
+
+.pager-container {
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
+
+  a {
+    color: @primary;
+    margin: 0 3px;
+    cursor: pointer;
+
+    &.disabled {
+      color: @lightWords;
+      cursor: not-allowed;
+    }
+
+    &.active {
+      color: @words;
+      font-weight: bold;
+      cursor: text;
     }
   }
-  </style>
+}
+</style>
