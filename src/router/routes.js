b01376e3ac8c0d13b39e36d3180b65066b53c76d
Damon <damon@smart-one.ca>
2023-09-29 14:59:07 -0400

    added router components

---
+++ src/router/routes.js
@@ -0,0 +1,33 @@
+import Home from "@/views/Home"
+import About from "@/views/About"
+import Blog from "@/views/Blog"
+import Message from "@/views/Message"
+import Project from "@/views/Project"
+
+export default [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/blog',
+      name: 'Home',
+      component: Blog
+    },
+    {
+      path: '/about',
+      name: 'Home',
+      component: About
+    },
+    {
+      path: '/projects',
+      name: 'Home',
+      component: Project
+    },
+    {
+      path: '/message',
+      name: 'Home',
+      component: Message
+    }
+  ]
